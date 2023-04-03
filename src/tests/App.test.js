bb6e2425b462b94eef4d7b56acbeecf03297b408
William Axelsson <william.3lg@gmail.com>
2023-04-03 15:59:40 +0200

    fix: :bug: fixed test to pass

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { Textarea } from "@chakra-ui/react";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textAreaElement = screen.getByRole("textbox", { text: /Api response will be displayed here/i });
+  expect(textAreaElement).toBeInTheDocument();
+  expect(textAreaElement.value).toEqual("Api response will be displayed here");
 });
